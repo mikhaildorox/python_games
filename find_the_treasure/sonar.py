3db3590fb280f5ecbd7eb3e8141e28a53f1e58ed
mikhaildoroshenko <mixdorosh@gmail.com>
2022-02-05 21:20:57 +0700

    game is done

--- find_the_treasure/sonar.py
+++ find_the_treasure/sonar.py
@@ -10,23 +10,23 @@ def getNewBoard():
     board = []
     for x in range(60):
         board.append([])
-    for y in range(15):
-        if random.randint(0, 1) == 0:
-            board[x].append('~')
-        else:
-            board[x].append('`')
+        for y in range(15):
+            if random.randint(0, 1) == 0:
+                board[x].append('~')
+            else:
+                board[x].append('`')
     return board
 
 
 def drawBoadr(board):
     # Draw new board
-    tensDigitsLine = ' '  # Create places for digits down in left side board
+    tensDigitsLine = '     '  # Create places for digits down in left side board
     for i in range(1, 6):
         tensDigitsLine += (' ' * 9) + str(i)
 
     # print digits in top side on board
     print(tensDigitsLine)
-    print(' ' + ('0123456789' * 6))
+    print('    ' + ('0123456789' * 6))
     print()
 
     # print 15 column
@@ -37,16 +37,16 @@ def drawBoadr(board):
         else:
             extraSpace = ''
 
-    # create srting for this column in board
-    boardRow = ''
-    for column in range(60):
-        boardRow += board[column][row]
+        # create setting for this column in board
+        boardRow = ''
+        for column in range(60):
+            boardRow += board[column][row]
 
-    print(f'{extraSpace}, {row}, {boardRow}, {row}')
+        print(f'{extraSpace} {row} {boardRow} {row}')
 
     # print digits in bottom side on board
     print()
-    print(' ' + ('0123456789' * 6))
+    print('    ' + ('0123456789' * 6))
     print(tensDigitsLine)
 
 
@@ -55,8 +55,8 @@ def getRandomChests(numChests):
     chests = []
     while len(chests) < numChests:
         newChest = [random.randint(0, 59), random.randint(0, 14)]
-    if newChest not in chests:  # to prove for chest was created
-        chests.append(newChest)
+        if newChest not in chests:  # to prove for chest was created
+            chests.append(newChest)
     return chests
 
 
@@ -72,15 +72,15 @@ def makeMove(board, chests, x, y):
     smallestDistance = 100  # all chests in distance < 100
     for cx, cy in chests:
         distance = math.sqrt((cx - x) * (cx - x) + (cy - y) * (cy - y))
+
         if distance < smallestDistance:
             smallestDistance = distance
-
-        smallestDistance = round(smallestDistance)
+            smallestDistance = round(smallestDistance)
 
         if smallestDistance == 0:
             # find the chest
             chests.remove([x, y])
-            return 'You find the treasure!'
+            return 'You find the chest!'
         else:
             if smallestDistance < 10:
                 board[x][y] = str(smallestDistance)
@@ -114,6 +114,7 @@ def showInstructions():
     You are a captain of the boat, who goes to the treasure. Your mission is find three chests with treasure
     with help of sonar. Sonar can define distance, but not направление. Input coord place for Sonar. Distance 
     for treasure was writing on map or was put X. On map C it`s a chests. 3 it`s distance for close chests.\
+                                   
                                     1           2           3           
                                 012345678901234567890123456789012       
                                 
@@ -132,6 +133,7 @@ def showInstructions():
     input()
 
     print('''If you find the chest 100%, you can take that. New sonar update information about chests.
+                                     
                                        1           2           3           
                                 012345678901234567890123456789012       
                                 
@@ -167,7 +169,7 @@ while True:
 
     while sonarDevices > 0:
         # Show sonar and chests
-        print(f'You have {sonarDevices} sonar. Not find {len(theChests)} chests.')
+        print(f'You have {sonarDevices} sonar. Not find chests {len(theChests)}.')
 
         x, y = enterPlayerMove(previousMove)
         previousMove.append([x, y])  # Watch moves for update sonars
@@ -179,13 +181,22 @@ while True:
                 # Update sonars on the map
                 for x, y in previousMove:
                     makeMove(theBoard, theChests, x, y)
-                    drawBoadr(theBoard)
-                    print(moveResult)
+
+            drawBoadr(theBoard)
+            print(moveResult)
 
             if len(theChests) == 0:
                 print('You find all treasure!')
                 break
 
-            sonarDevices -= 1
-            if sonarDevices == 0:
-                print('8')
+        sonarDevices -= 1
+
+    if sonarDevices == 0:
+        print('You have no Sonar. Game over.')
+        print('Treasure was in:')
+        for x, y in theChests:
+            print(f'{x},{y}')
+
+    print('Play again? (y/n)')
+    if not input().lower().startswith('y'):
+        sys.exit()
